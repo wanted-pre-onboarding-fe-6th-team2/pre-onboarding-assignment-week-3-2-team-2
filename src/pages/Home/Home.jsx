c4810015e255427e63089c33e59ad6b7716e0678
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-18 03:18:15 +0900

    Chore: useSelector를 활용해 데이터 가져오는 예시 코드 작성
    - 기존 useState, useEffect로 작성되어 있던 상태 관리 로직을 Redux Toolkit 로직으로 변경

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,13 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import CommentList from '@/components/home/CommentList/CommentList';
 import Form from '@/components/home/Form/Form';
 import PageList from '@/components/home/PageList/PageList';
-import { getCommentsThunk } from '@/actions/comments';
-import {
-  getCommentThunk,
-  addCommentThunk,
-  updateCommentThunk,
-  deleteCommentThunk,
-} from '@/actions/comment';
+import { getCommentsThunk } from '@/store/comments';
 
 const Home = () => {
   const isLoading = useSelector(state => state.comments.isLoading);
@@ -21,57 +15,12 @@ const Home = () => {
     dispatch(getCommentsThunk());
   }, [dispatch]);
 
-  const handleAddClick = () => {
-    dispatch(
-      addCommentThunk({
-        profileUrl: 'https://picsum.photos/id/22/50/50',
-        author: 'abc_22',
-        content: 'REEEEEEEEEEEEEEEEEEEEEDUX',
-        createdAt: '2022-03-22',
-      })
-    );
-  };
-
-  const handleGetClick = () => {
-    dispatch(getCommentThunk(22));
-  };
-
-  const handleUpdateClick = () => {
-    dispatch(
-      updateCommentThunk({
-        commentId: 1,
-        comment: {
-          profileUrl: 'https://picsum.photos/id/22/50/50',
-          author: 'abc_22',
-          content: 'REEEEEEEEEEEEEEEEEEEEEDUX',
-          createdAt: '2022-03-22',
-        },
-      })
-    );
-  };
-
-  const handleDeleteClick = () => {
-    dispatch(deleteCommentThunk(22));
-  };
-
   if (isLoading) return <h1>로딩 중입니다...</h1>;
   if (error) return <h1>{error.message}</h1>;
   if (!data) return null;
 
   return (
     <>
-      <button type="button" onClick={handleAddClick}>
-        추가 하기
-      </button>
-      <button type="button" onClick={handleGetClick}>
-        하나 가져오기
-      </button>
-      <button type="button" onClick={handleUpdateClick}>
-        하나 수정하기
-      </button>
-      <button type="button" onClick={handleDeleteClick}>
-        삭제하기
-      </button>
       <CommentList commentList={data} />
       <PageList />
       <Form />
