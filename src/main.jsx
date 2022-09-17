629d1ad0fa65a78fcd2b4aab20d49771e5a66b7f
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-17 23:09:16 +0900

    Chore: 컨벤션에 맞는 import 경로 수정

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from '@/App';
-import store from './store';
+import store from '@/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
