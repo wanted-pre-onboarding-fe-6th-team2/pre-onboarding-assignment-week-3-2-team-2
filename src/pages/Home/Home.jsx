3e86868a7a9799bc68a92bb14023ca5de7921d0c
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-17 23:55:19 +0900

    Fix: ESLint "no-param-reassign" option 문제
    - immerjs 내장된 RTK 기능 사용 불가능으로 끔

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,78 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import CommentList from '@/components/home/CommentList/CommentList';
 import Form from '@/components/home/Form/Form';
 import PageList from '@/components/home/PageList/PageList';
-import commentsApiService from '@/api/commentsApiService';
+import { getCommentsThunk } from '@/actions/comments';
+import {
+  getCommentThunk,
+  addCommentThunk,
+  updateCommentThunk,
+  deleteCommentThunk,
+} from '@/actions/comment';
 
 const Home = () => {
-  const [commentList, setCommentList] = useState([]);
+  const isLoading = useSelector(state => state.comments.isLoading);
+  const data = useSelector(state => state.comments.data);
+  const error = useSelector(state => state.comments.error);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchComments = async () => {
-      try {
-        const commentsResponse = await commentsApiService.getComments();
+    dispatch(getCommentsThunk());
+  }, [dispatch]);
 
-        setCommentList(commentsResponse);
-      } catch (error) {
-        throw new Error(error);
-      }
-    };
+  const handleAddClick = () => {
+    dispatch(
+      addCommentThunk({
+        profileUrl: 'https://picsum.photos/id/22/50/50',
+        author: 'abc_22',
+        content: 'REEEEEEEEEEEEEEEEEEEEEDUX',
+        createdAt: '2022-03-22',
+      })
+    );
+  };
 
-    fetchComments();
-  }, []);
+  const handleGetClick = () => {
+    dispatch(getCommentThunk(22));
+  };
+
+  const handleUpdateClick = () => {
+    dispatch(
+      updateCommentThunk({
+        commentId: 1,
+        comment: {
+          profileUrl: 'https://picsum.photos/id/22/50/50',
+          author: 'abc_22',
+          content: 'REEEEEEEEEEEEEEEEEEEEEDUX',
+          createdAt: '2022-03-22',
+        },
+      })
+    );
+  };
+
+  const handleDeleteClick = () => {
+    dispatch(deleteCommentThunk(22));
+  };
+
+  if (isLoading) return <h1>로딩 중입니다...</h1>;
+  if (error) return <h1>{error.message}</h1>;
+  if (!data) return null;
 
   return (
     <>
-      <CommentList commentList={commentList} />
+      <button type="button" onClick={handleAddClick}>
+        추가 하기
+      </button>
+      <button type="button" onClick={handleGetClick}>
+        하나 가져오기
+      </button>
+      <button type="button" onClick={handleUpdateClick}>
+        하나 수정하기
+      </button>
+      <button type="button" onClick={handleDeleteClick}>
+        삭제하기
+      </button>
+      <CommentList commentList={data} />
       <PageList />
       <Form />
     </>
