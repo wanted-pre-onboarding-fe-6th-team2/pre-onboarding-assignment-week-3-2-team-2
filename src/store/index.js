d7a0a21a412cc7f660d486c2ec88d447c7f7293d
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-17 23:34:50 +0900

    Feat: RTK - Store 구현
    - configureStore로 Store와 관련 설정 추가
    - logger 추가
    - devtools 설정

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,25 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import commentReducer from '@/reducers/commentReducer';
-
-const logger = createLogger();
-
-const store = createStore(commentReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
-
-export default store;
-
-// RTK Store
-// import { configureStore } from '@reduxjs/toolkit';
+// import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import { createLogger } from 'redux-logger';
+// import { composeWithDevTools } from '@redux-devtools/extension';
 // import commentReducer from '@/reducers/commentReducer';
 
-// const isDev = process.env.NODE_ENV === 'development';
+// const logger = createLogger();
 
-// const store = configureStore({
-//   reducer: { comment: commentReducer },
-//   devTools: isDev,
-// });
+// const store = createStore(commentReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
 
 // export default store;
+
+// RTK Store
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import commentsReducer from '@/reducers/commentsSlice';
+import commentReducer from '@/reducers/commentSlice';
+
+const store = configureStore({
+  reducer: { comments: commentsReducer, comment: commentReducer },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export default store;
