0ed4318b2c0124aad35ab794cf9eb032ded999a3
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-18 01:22:08 +0900

    Fix: Redux 로직 삭제 및 RTK로 변경
    -  내장된 기능이 효율적인 RTK 사용

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,3 @@
-// import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import { createLogger } from 'redux-logger';
-// import { composeWithDevTools } from '@redux-devtools/extension';
-// import commentReducer from '@/reducers/commentReducer';
-
-// const logger = createLogger();
-
-// const store = createStore(commentReducer, composeWithDevTools(applyMiddleware(thunk, logger)));
-
-// export default store;
-
-// RTK Store
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import commentsReducer from '@/reducers/commentsSlice';
